18be00d434356bc641a886fa246a9973bc7c7875
azukisiromochi <azukisiromochi@gmail.com>
2022-01-27 15:01:36 +0900

    🚧 Update page using Vuesax

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
  import Vuesax from 'vuesax'
 
- import 'vuesax/dist/vuesax.css'
- Vue.use(Vuesax, {
+Vue.use(Vuesax, {
    // colors: {
    //    primary:'#5b3cc4',
    //    success:'rgb(23, 201, 100)',
